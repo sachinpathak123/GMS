a6c6ab65a4ae4e7a06fba904954edcc4fda01eb3
Ayush7454 <ayushgaya7454@gmail.com>
2024-01-21 00:14:34 +0530

    allignment

--- src/app/pages/Why-GetMax/Newstat.tsx
+++ src/app/pages/Why-GetMax/Newstat.tsx
@@ -7,12 +7,13 @@ const Newstat = () => {
     <br />
     <br />
     <div className=" flex flex-col items-center justify-end text-4xl text-black font-red-hat-display space-y-[-px]">
-      <b className="w-[293px] text-[25px] mb-[-50px]">
+      <b className="w-[220px] text-[25px] mb-[-50px]">
         WHY CHOOSE US
       </b>
       <div className="w-[377.4px] h-[112.5px] flex flex-col items-center justify-center py-[.25px]  box-border gap-[21px] mt-[-5px] text-[40px] font-source-sans-pro">
-       <div className="w-[282px] relative [background:radial-gradient(50%_50%_at_50%_50%,_#632ed3,_rgba(66,_133,_244,_0))] h-2.5" />
-        <b className="w-[377.3px] relative tracking-[0.04em] mb-[-80px]">
+       <div className="w-[290px] relative [background:radial-gradient(50%_50%_at_50%_50%,_#632ed3,_rgba(66,_133,_244,_0))] h-2.5" />
+        <b className="w-[309
+          px]  relative tracking-[0.04em] mb-[-80px]">
           <span>OUR</span>
           <span className="text-blueviolet"> STATISTICS</span>
         </b>
