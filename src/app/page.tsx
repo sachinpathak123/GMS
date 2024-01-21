b43546603087ca5f89d704faaba1332f2a4b3747
sahil <sahilhussain1231@gmail.com>
2024-01-21 14:22:54 +0530

    completed our softwares part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,22 @@ import Division from './pages/Navbar/Division'
 import Homesection from './Components/Homesection'
 import Territory from './pages/Home/Territory'
 import Territorycircles from './pages/Home/Territorycircles'
+import Softwarecard from './Components/Cards/Softwarecard'
+import './pages/Home/software.css'
+import Oursoftware from './pages/Home/Oursoftware'
+    
+
 const page = () => {
   return (
     <div>
-      <Navbar />
-      <Division />
-      <Home />
-      <Homesection />
-      <div className="container">
-      <Territorycircles />
-</div>
-
+        <Navbar />
+        <Division />
+        <Home />
+        <Homesection />
+        <div className="container mb-36">
+          <Territorycircles />
+        </div>
+        <Oursoftware/>
     </div>
   )
 }
