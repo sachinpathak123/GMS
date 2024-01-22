4e60483b96940c34efcffce1621d4505371dd01f
sahil <sahilhussain1231@gmail.com>
2024-01-22 13:39:06 +0530

    fixed merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,9 @@ import Division from './pages/Navbar/Division'
 import Homesection from './Components/Homesection'
 import Territory from './pages/Home/Territory'
 import Territorycircles from './pages/Home/Territorycircles'
-<<<<<<< HEAD
 import Software from './pages/Home/Oursoftwares/Software'
-=======
-import Softwarecard from './Components/Cards/Softwarecard'
-import './pages/Home/software.css'
-import Oursoftware from './pages/Home/Oursoftware'
 import Contact from './pages/Home/Contact'
     
->>>>>>> 3a7907ab666cd9dd4188cce712a605db2f8d24b2
 
 const page = () => {
   return (
@@ -26,12 +20,8 @@ const page = () => {
         <div className="container mb-36">
           <Territorycircles />
         </div>
-<<<<<<< HEAD
         <Software/>
-=======
-        <Oursoftware/>
         <Contact />
->>>>>>> 3a7907ab666cd9dd4188cce712a605db2f8d24b2
     </div>
   )
 }
