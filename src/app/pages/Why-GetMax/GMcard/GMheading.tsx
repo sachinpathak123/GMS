caa9c81fce002f2fdd6e93d86ff54b4a50146815
Ayush7454 <ayushgaya7454@gmail.com>
2024-01-24 21:39:09 +0530

    commiting

--- src/app/pages/Why-GetMax/GMcard/GMheading.tsx
+++ src/app/pages/Why-GetMax/GMcard/GMheading.tsx
@@ -1,5 +1,8 @@
+"use client"
 import React from 'react'
 import { url } from 'inspector'
+import {motion} from 'framer-motion'
+
 const GMheading = () => {
   return (
     <div className=''>
@@ -8,7 +11,6 @@ const GMheading = () => {
         PROCESS AND PROCEDURE
       </h6>
       </div>
-      <br />
       <div>
         <p className='text-center font-extrabold text-3xl'>
           WHICH PROMISE <span className='text-custom-purple'>DESIRED</span> RESULT
@@ -16,14 +18,16 @@ const GMheading = () => {
       </div>
      <br /><br />
       <div className='grid grid-cols-4 place-content-center mx-24 px-24'>
-        <div id="first" className='py-24 px-0 border border-1 rounded-xl text-center' style={{
+        <motion.div id="first" className='py-24 px-0 border border-1 rounded-xl text-center' style={{
           backgroundImage:'url("/first.png")',
           backgroundSize:'cover',
           width:'85%',
           height:'110%',
-          }}>
+          }}
+       whileHover={{ scale: 1.1 }}
+          >
         <h2 className='text-center text-white'>STEP ONE</h2>
-        </div>
+        </motion.div>
         <div id="second" className='py-24 px-0 border border-1 rounded-xl' style={{
           backgroundImage:'url("/first.png")',
           backgroundSize:'cover',
@@ -49,8 +53,10 @@ const GMheading = () => {
         <h2 className='text-center text-white'>STEP ONE</h2>
         </div>
       </div>
+      <br />
+      <br />
     </div>
   )
 }
 
-export default GMheading
+export default GMheading
