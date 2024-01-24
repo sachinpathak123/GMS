5e0f47a5ef7897d0be0821d3a22d2365c52ee064
Sachin <pathaksachin876@gmail.com>
2024-01-24 11:51:25 +0530

    pulling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Homesection from './Components/Homesection'
 import Territorycircles from './pages/Home/Territorycircles'
 import Software from './pages/Home/Oursoftwares/Software'
 import Contact from './pages/Home/Contact'
+import Final from './pages/Home/Final'
     
 
 const page = () => {
@@ -20,6 +21,7 @@ const page = () => {
         <Software/>
         <Contact />
         {/* <Oursoftware/> */}
+         
     </div>
   )
 }
