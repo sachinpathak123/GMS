84d2bdddd8001b3cbc5c5e9f9e85b7f836ab4567
Ayush7454 <ayushgaya7454@gmail.com>
2024-01-25 19:11:16 +0530

    commiting

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 "use client"
 import React from 'react'
 
+
 const Home = () => {
   return (
     <>
-    <div className=' w-screen h-screen flex items-center justify-center text-custom-purple font-sans m-0 p-0 overflow-x-hidden '>
+    <div className='overflow-hidden w-screen h-screen flex items-center justify-center text-custom-purple font-sans m-0 p-0 '>
         <div className='z-10 relative left-96'>
         <div className='font-extrabold text-5xl text-center space-x-0 '>
             <span>Building Better Products</span>
@@ -17,29 +18,29 @@ const Home = () => {
       <button className=' px-6 py-2 bg-purple-700 text-white rounded-md  capitalize font-thin flex flex-rowhover:bg-purple-800'>Let's get started!
      
       </button>
-      
+     
             
       </div>
       </div>
       
-      <div className=" h-[721px] z-0 ">
+      <div className="w-screen h-[721px] z-0 overflow-hidden">
       <img
-        className="absolute top-[200px] left-[-40px] w-[94.7px] h-[90.2px] object-contain"
+        className="absolute top-[200px] left-[-40px] w-[94.7px] h-[90.2px] object-contain "
         alt=""
         src="/yellowcircle.png"
       />
       <img
-        className="absolute top-[550px] left-[-22.6px] w-[46.1px] h-[43.8px] object-contain"
+        className="absolute top-[450px] left-[-22.6px] w-[46.1px] h-[43.8px] object-contain"
         alt=""
         src="/purplecircle.png"
       />
-       <img
-        className="absolute top-[400px] left-[-40px] w-[94.7px] h-[90.2px] object-contain"
+        <img
+        className="absolute top-[550px] left-[400px] w-[46px] h-[46px] object-contain "
         alt=""
         src="/yellowcircle.png"
       />
       <img
-        className="absolute top-[621.6px] right-[-50px] w-[104.5px] h-[250px] object-contain"
+        className="absolute top-[621.6px] right-[-50px] w-1/12 h-[250px] object-contain overflow-hidden "
         alt=""
         src="/yellowcircle.png"
       />
