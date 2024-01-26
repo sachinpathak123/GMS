f6a0d13f2ef8306ea60eeeb0aa4ea6f88219fe8e
Ayush7454 <ayushgaya7454@gmail.com>
2024-01-26 17:14:45 +0530

    changes

--- src/app/pages/Home/Outcome.tsx
+++ src/app/pages/Home/Outcome.tsx
@@ -9,7 +9,7 @@ const Outcome = () => {
         <br/>
         <h2 className='text-center text-purple-700 text-[35px] font-semibold'>Quality Outcomes. </h2>
        <div className='grid grid-cols-3 gap-y-20 gap-x-11 px-24 py-20  justify-items-center   '>
-           <Qualities imgsource= "help 100+.png" data="we help to transform performance of revenue cycle to deliver improved margins,increased cash flow" head="Help 100+  healthcare organisation"/>
+           <Qualities imgsource= "slider.png" data="we help to transform performance of revenue cycle to deliver improved margins,increased cash flow" head="Help 100+  healthcare organisation"/>
            <Qualities imgsource= "patient.png" data="Our well trained RCM team and appropriate technology may aid in making billing and coding process seamless and efficient.Patient will be less worried." head="Improves Patient Experience"/>
            <Qualities imgsource= "reduceclaim.png" data="Implementing RCM process helps minimize errors in claims submission,leading to a decrease in claim denials and subsequent rework. " head="Reduces Claim Denials"/>
            <Qualities imgsource= "coupon.png" data="we cut cost by 40% by outsourcing coding denials management and avoid overspending and preventing costly malfunctions." head="40% Cost Cuttings"/>
