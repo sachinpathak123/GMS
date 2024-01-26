589e4289b77ecb21dc4724ec10ea54a72720a252
sachinpathak123 <gohilkinnari2002@gmail.com>
2024-01-26 17:30:02 +0530

    compare page added

---
+++ src/app/pages/Why-GetMax/compare/compare.tsx
@@ -0,0 +1,155 @@
+import React , { FC } from 'react';
+import Container from "@mui/material/Container";
+
+function compare() {
+  return (
+    <> 
+    <div>
+      <p className="font-bold text-2xl text-center">NEED ANY MORE OF A REASON</p>
+      <div style={{width:"550px",height:"5px",borderRadius:"25%",background: 'linear-gradient(to right, transparent, #9f00c5, transparent)',position:"absolute",left:"400px",top:"50px"}}></div>
+      <div className='flex'>
+        <Container>
+          <div style={{ width:"400px",backgroundColor: "#9f00c5", height: "482px", borderRadius: "30px",position:"absolute",left:"90px",top:"120px"}}>
+            <Container>
+              <div style={{ backgroundColor: "#CF9FFF",width: "350px", height: "482px", borderRadius: "30px"}}>
+                <p className="font-bold text-2xl absolute top-10 right-20">GETMAX SOLUTIONS</p>
+                <div className='absolute top-32'>
+                    <table>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                </table>
+              </div>
+              </div>
+            </Container>
+          </div>
+        </Container>
+        <Container>
+          <div style={{width:"360px", backgroundColor: "#B2BEB5", height: "400px", borderRadius: "50px", margin: "20px",border:"2px solid #848884" ,position:"absolute",top:"140px",left:"470px"}}>
+            <p className="text-purple-900 font-bold text-2xl absolute left-16 top-16">MEDICAL BILLERS</p>
+            <div className='absolute top-32'>
+                <table>
+            <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                </table>
+              </div>
+                  <button style={{position:"absolute",top:"300px"}} className="bg-red-600 text-white text-center w-48 h-12 absolute left-20 font-bold">ONLY FOR BENEFITS</button>
+          </div>
+        </Container>
+        <Container>
+          <div style={{ width:"400px",backgroundColor: "#9f00c5", height: "482px", borderRadius: "30px",position:"absolute",left:"850px",top:"120px"}}>
+            <Container>
+              <div style={{ backgroundColor: "#CF9FFF",width: "350px", height: "482px", borderRadius: "30px"}}>
+                <p className="font-bold text-2xl absolute top-10 right-20">GETMAX SOLUTIONS</p>
+                <div className='absolute top-32'>
+                <table>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                <td className='flex items-center mb-2'>
+                  <div className="bg-green-500 rounded-full w-7 h-7 p-2 mr-2 ml-10">
+                    <img className="w-full h-full rounded-full" src={process.env.PUBLIC_URL + '/tick.png'} alt="Tick not found" />
+                  </div>
+                    <span className="text-1xl ml-2 text-white font-bold">Certifies Coders</span>
+                </td>
+                </table>
+              </div>
+              </div>
+            </Container>
+          </div>
+        </Container>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default compare;
