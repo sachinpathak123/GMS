a7e914a761aadc7372a6985b3108ab863ab3584d
Keshav <keshavbhutaani1360@gmail.com>
2024-01-26 20:53:07 +0530

    Added circular section in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home'
 import Navbar from './pages/Navbar/Navbar'
 import Division from './pages/Navbar/Division'
 import Homesection from './Components/Homesection'
+import Final from '@/app/pages/Home/Final'
 import Territorycircles from './pages/Home/Territorycircles'
 import Software from './pages/Home/Oursoftwares/Software'
 import Contact from './pages/Home/Contact'
@@ -16,6 +17,7 @@ const page = () => {
         <Home />
         <Homesection />
           <Territorycircles />
+        <Final />
         <Software/>
         <Contact />
         {/* <Oursoftware/> */}
