92279d429fd7bde5e51c015d6f56d61d54ed39d4
Sachin <pathaksachin876@gmail.com>
2024-02-04 19:41:57 +0530

    changed imports

--- src/app/pages/RCM-Services/App.tsx
+++ src/app/pages/RCM-Services/App.tsx
@@ -1,13 +1,13 @@
 
 import React from 'react';
 import './App.css';
-import Hexagon from './components/Hexagon';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Principle from './components/principle';
-import How from './components/how';
-import Sidebar from './components/sidebar';
-import MainPage from './components/mainpage';
+import Hexagon from './Hexagon';
+import Header from './Header';
+import Footer from './Footer';
+import Principle from './principle';
+import How from './how';
+import Sidebar from './sidebar';
+import MainPage from './mainpage';
 
 function App() {
   return (
