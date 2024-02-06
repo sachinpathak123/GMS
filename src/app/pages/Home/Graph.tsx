3f5635733f60e3caefb6576b2f03d54e6e35122b
Sachin <pathaksachin876@gmail.com>
2024-02-07 00:29:45 +0530

    ...

--- src/app/pages/Home/Graph.tsx
+++ src/app/pages/Home/Graph.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Graph = () => {
   return (
-    <div className='bg-lp-purple w-screen h-full'>
+    <div className='bg-white w-screen h-full py-8'>
       <div className="soft-header" id="soft-mainHeader">
    
    <p className="soft-head1 font-extrabold">A trusted partner</p>
- <p className='text-black text-3xl text-center'>for organizing committed to improving the RCM system through technology</p>
+ <p className='text-black text-3xl py-5 text-center'>for organizing committed to improving the RCM system through technology</p>
  </div>
     <div className='flex items-center justify-center'>
     <img src="/graph.png" alt="an image" className='w-1/2 h-1/2 py-16' />
