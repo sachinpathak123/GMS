0467c0105ec7aa54ec492ff6e664db30e8b31fc6
Keshav <keshavbhutaani1360@gmail.com>
2024-02-09 17:17:48 +0530

    fixed some merge conflicts

--- src/app/pages/Home/Final.tsx
+++ src/app/pages/Home/Final.tsx
@@ -1,49 +1,105 @@
-import React from 'react'
-import SlideCard from './SlideCard'
+import React from "react";
+import SlideCard from "./SlideCard";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 // import 'swiper/swiper-bundle.min.css';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { Autoplay, A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
-
-
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import {
+  Autoplay,
+  A11y,
+  Navigation,
+  Pagination,
+  Scrollbar,
+} from "swiper/modules";
 
 const Final = () => {
   return (
-    
-    
-    <div className='bg-[#FBF8FF] h-screen max-w-screen '>
-         <div className='flex flex-col gap-2'>
-        <h1 className='font-inter text-center text-5xl font-bold text-custom-purple py-4 mt-8'><span className='text-custom-purple'>RCM</span> Services</h1>
-        <p className='font-inter text-center text-md font-thin'>Discover our tailored RCM services, meticulously designed to amplify your practice's financial efficiency and ensure seamless revenue management</p>
+    <div className="bg-[#FBF8FF] h-screen max-w-screen ">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-inter text-center text-5xl font-bold text-custom-purple py-4 mt-8">
+          <span className="text-custom-purple">RCM</span> Services
+        </h1>
+        <p className="font-inter text-center text-md font-thin">
+          Discover our tailored RCM services, meticulously designed to amplify
+          your practice's financial efficiency and ensure seamless revenue
+          management
+        </p>
       </div>
-     <div className='swiper-container' style={{ margin: '4rem 9rem' }}> 
-     <Swiper
-    //  install Swiper modules
-      modules={[Autoplay,Navigation, Pagination, Scrollbar, A11y]}
-      slidesPerView={3}
-    //   navigation
-      pagination={{ clickable: true }}
-    //   scrollbar={{ draggable: true }}
-     // autoplay={{ delay: 1000, disableOnInteraction: false }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      
-    >
-      <SwiperSlide> <SlideCard heading="Billing section" content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?" headimg="/ufo.png" /> </SwiperSlide>
-      <SwiperSlide> <SlideCard heading="Medical Coding" content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?" headimg="/ufo.png" /> </SwiperSlide>
-      <SwiperSlide> <SlideCard heading="Backend Solutions" content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?" headimg="/ufo.png" /> </SwiperSlide>
-      <SwiperSlide> <SlideCard heading="Medical Coding" content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?" headimg="/ufo.png" /> </SwiperSlide>
-      <SwiperSlide> <SlideCard heading="Medical Coding" content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?" headimg="/ufo.png" /> </SwiperSlide>
-      <SwiperSlide> <SlideCard heading="Medical Coding" content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?" headimg="/ufo.png" /> </SwiperSlide>
-      <SwiperSlide> <SlideCard heading="Medical Coding" content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?" headimg="/ufo.png" /> </SwiperSlide>
-      </Swiper>
-  </div>
-    </div> 
-  )
-}
+      <div className="swiper-container" style={{ margin: "4rem 9rem" }}>
+        <Swiper
+          //  install Swiper modules
+          modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView={3}
+          //   navigation
+          pagination={{ clickable: true }}
+          //   scrollbar={{ draggable: true }}
+          // autoplay={{ delay: 1000, disableOnInteraction: false }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide>
+            {" "}
+            <SlideCard
+              heading="Billing section"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?"
+              headimg="/ufo.png"
+            />{" "}
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <SlideCard
+              heading="Medical Coding"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?"
+              headimg="/ufo.png"
+            />{" "}
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <SlideCard
+              heading="Backend Solutions"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?"
+              headimg="/ufo.png"
+            />{" "}
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <SlideCard
+              heading="Medical Coding"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?"
+              headimg="/ufo.png"
+            />{" "}
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <SlideCard
+              heading="Medical Coding"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?"
+              headimg="/ufo.png"
+            />{" "}
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <SlideCard
+              heading="Medical Coding"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?"
+              headimg="/ufo.png"
+            />{" "}
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <SlideCard
+              heading="Medical Coding"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit.debitis explicabo, architecto soluta doloremque?"
+              headimg="/ufo.png"
+            />{" "}
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
 
 export default Final;
--- src/app/pages/Home/Slider.tsx
+++ src/app/pages/Home/Slider.tsx
@@ -1,48 +1,82 @@
-import { motion } from 'framer-motion';
-import React, { useState,useEffect } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxCircle } from 'react-icons/rx';
-import { TbCircleFilled } from 'react-icons/tb';
-import {Autoplay, Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { motion } from "framer-motion";
+import React, { useState, useEffect } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxCircle } from "react-icons/rx";
+import { TbCircleFilled } from "react-icons/tb";
+import {
+  Autoplay,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+} from "swiper/modules";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import Slidercomp from './Slidercomp';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import Slidercomp from "./Slidercomp";
 function Slider() {
   return (
     <>
-    <div className="font-inter text-center bg-white m-8">
-   
-   <p className="text-custom-purple text-6xl font-extrabold">Case Studies</p>
-   <br />
-   <p className='text-sm text-black px-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laudantium temporibus illo incidunt officia quae, deserunt harum necessitatibus ipsum. <br />Velit architecto ab magnam saepe molestias, neque quis nesciunt suscipit eos?Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />Officiis numquam impedit voluptates nemo doloremque repellat ullam exercitationem atque praesentium deleniti.</p>
-
- </div>
- <Swiper style={{ margin: '0rem 15rem' }}
-      // install Swiper modules
-      modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={1}
-    //  navigation
-      pagination={{ clickable: true }}
-      autoplay={{ delay: 1000, disableOnInteraction: false }}
-    //  scrollbar={{ draggable: true }}
-      speed={1000}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide><Slidercomp headimg="slide1.png" content="A Semiautomatic Multi-Label Color Image Segmentation Coupling Dirichlet Problem and Colour Distances" /></SwiperSlide>
-      <SwiperSlide><Slidercomp headimg="slide1.png" content="A Semiautomatic Multi-Label Color Image Segmentation Coupling Dirichlet Problem and Colour Distances" /></SwiperSlide>
-      <SwiperSlide><Slidercomp headimg="slide1.png" content="A Semiautomatic Multi-Label Color Image Segmentation Coupling Dirichlet Problem and Colour Distances" /></SwiperSlide>
-      <SwiperSlide><Slidercomp headimg="slide1.png" content="A Semiautomatic Multi-Label Color Image Segmentation Coupling Dirichlet Problem and Colour Distances" /></SwiperSlide>
-    </Swiper>
-
- 
+      <div className="font-inter text-center bg-white m-8">
+        <p className="text-custom-purple text-6xl font-extrabold">
+          Case Studies
+        </p>
+        <br />
+        <p className="text-sm text-black px-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          laudantium temporibus illo incidunt officia quae, deserunt harum
+          necessitatibus ipsum. <br />
+          Velit architecto ab magnam saepe molestias, neque quis nesciunt
+          suscipit eos?Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          <br />
+          Officiis numquam impedit voluptates nemo doloremque repellat ullam
+          exercitationem atque praesentium deleniti.
+        </p>
+      </div>
+      <Swiper
+        style={{ margin: "0rem 0rem" }}
+        // install Swiper modules
+        modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        //  navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 1000, disableOnInteraction: false }}
+        //  scrollbar={{ draggable: true }}
+        speed={1000}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <Slidercomp
+            headimg="slide1.png"
+            content="A Semiautomatic Multi-Label Color Image Segmentation Coupling Dirichlet Problem and Colour Distances"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slidercomp
+            headimg="slide1.png"
+            content="A Semiautomatic Multi-Label Color Image Segmentation Coupling Dirichlet Problem and Colour Distances"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slidercomp
+            headimg="slide1.png"
+            content="A Semiautomatic Multi-Label Color Image Segmentation Coupling Dirichlet Problem and Colour Distances"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slidercomp
+            headimg="slide1.png"
+            content="A Semiautomatic Multi-Label Color Image Segmentation Coupling Dirichlet Problem and Colour Distances"
+          />
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 }
